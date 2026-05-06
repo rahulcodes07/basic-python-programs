1af09245e436d67af1e9ec23661ba46f1a62cccb
Rahul Raj <rahulsam7368@gmail.com>
2026-05-06 17:00:47 +0530

    New File

---
+++ python1.py
@@ -0,0 +1,37 @@
+#    ARITHMETIC OPERATIONS
+
+
+int_1 = 2
+int_2 = 6
+
+sum_int = int_1 + int_2
+#print (sum_int)
+
+int_3 = 9
+int_4 = 5
+
+#print(f'The difference is : {int_3- int_4}')
+
+float_1 = 3.14
+float_2 = 2.72
+#print(type(float_1))
+
+#print(f' The addition of the above two floats equals to: {float_1+float_2}')
+#print(f'The addition of int_3 and float_2 equals to: {int_3+float_1}')
+
+#print(int_3%int_1)  #For remainder
+#print(int_3//int_1) #To extract the closest or equal number to the result
+#print(int_3**int_1) #Calculate the number 1 to the power of number 3
+
+#converted_float = float(int_2)
+#print(converted_float, type(converted_float)) #For converting any variable type.
+
+#rounded_float_1 = round(float_1)
+#print(rounded_float_1)
+
+#int_4 -= 20
+#print(int_4)
+
+greeting = "Hii, "
+greeting -= "how are you?"
+print(greeting)
